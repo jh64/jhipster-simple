f281d00d7ae49fa45069466c70b876a79c88b02f
jh64 <48374211+jh64@users.noreply.github.com>
2020-11-15 14:35:56 +1100

    put in comment

--- ocp/ocp-apply.sh
+++ ocp/ocp-apply.sh
@@ -2,6 +2,10 @@
 # Use this script to run oc commands to create resources in the selected namespace. Files are ordered
 # in proper order. 'oc process' processes the template as resources which is again piped to
 # 'oc apply' to create those resources in OpenShift namespace
-oc process -f ../ocp/registry/scc-config.yml | oc apply -f -
-oc process -f ../ocp/jhipstersimple/jhipstersimple-postgresql.yml | oc apply -f -
-oc process -f ../ocp/jhipstersimple/jhipstersimple-deployment.yml | oc apply -f -
+
+echo run these 2 lines first
+#oc process -f ../ocp/registry/scc-config.yml | oc apply -f -
+#oc process -f ../ocp/jhipstersimple/jhipstersimple-postgresql.yml | oc apply -f -
+
+echo then run this line
+#oc process -f ../ocp/jhipstersimple/jhipstersimple-deployment.yml | oc apply -f -
